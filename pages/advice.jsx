d43e95c7740013b9e1ce294607f8be79403b6147
countventa <tiagovicjms@gmail.com>
2025-11-27 20:51:38 +0100

    My advice

--- pages/advice.jsx
+++ pages/advice.jsx
@@ -3,7 +3,26 @@ export default function MesConseils() {
     <div style={{ padding: "20px" }}>
       <h1>Mes conseils</h1>
       <p>
-        Ici tu peux mettre le texte, les informations ou les conseils que tu veux afficher.
+      These tips are personal advice, meaning they may vary depending on the individual. I’m sharing them to help you out! 😉
+      
+        I / My Mass Gain Plan
+            • 🍽️ 3000 kcal/day — 293g carbs, 150g protein, 129g fat
+            (Calories distribution: Breakfast 15%, Lunch 30%, Dinner 35%, Snacks 20%)
+            • 💧 2.5L (minimum) of water per day
+            • 🚶‍♂️ 10,000 steps per day
+
+
+        II / Supplements (optional but useful)
+            • ⚡ Creatine: 7g/day → post-workout
+            • 🥤 Whey Gainer: 100g + water → post-workout
+            • 🌿 Ashwagandha (if stressed, anxious, or having sleep issues): 500mg before bed
+
+
+        III / Rest
+            🛌 Rest is extremely important for building muscle — no rest = no muscle growth.
+            I recommend getting at least 8 hours of sleep per night (minimum).
+
+            ⏱️ During your workout, take at least 2 min 30 sec of rest between sets.
       </p>
     </div>
   );
