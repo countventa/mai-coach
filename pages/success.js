bc8eba6a27dc58b4fa6edb53f1724592439c2d85
countventa <tiagovicjms@gmail.com>
2025-12-03 15:53:41 +0100

    PDF V2

--- pages/success.js
+++ pages/success.js
@@ -3,7 +3,7 @@ export default function Success(){
     <div className="container">
       <h2>Paiement réussi</h2>
       <p>Merci — ton ebook est disponible.</p>
-      <p><a className="button" href="/Venta_5-7_Workout.pdf">Télécharger le PDF</a></p>
+      <p><a className="button" href="/Venta 5_7 Workout (FINAL).pdf">Télécharger le PDF</a></p>
     </div>
   )
 }
